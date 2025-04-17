bd595dbd8362089b15d56d2ac492bc8bedb4356b
Aluno São Lucas <aluno.saolucas@ensino.ulbra.net>
2025-04-16 22:23:04 -0300

    primeiro commit

---
+++ js/script.js
@@ -0,0 +1,49 @@
+function somar() {
+    const v1 = Number(document.getElementById("valor1").value);
+    const v2 = Number(document.getElementById("valor2").value);
+    const resultado = v1 + v2;
+
+    document.getElementById("resultado").innerText = `Resultado: ${resultado}`;
+}
+
+function subtrair(){
+    const v1 = Number(document.getElementById("valor1").value);
+    const v2 = Number(document.getElementById("valor2").value);
+    const resultado = v1 - v2;
+
+    document.getElementById("resultado").innerText = `Resultado: ${resultado}`;
+}
+
+
+function multiplicar(){
+    const v1 = Number(document.getElementById("valor1").value);
+    const v2 = Number(document.getElementById("valor2").value);
+    const resultado = v1 * v2;
+
+    document.getElementById("resultado").innerText = `Resultado: ${resultado}`;
+}
+
+function dividir(){
+    const v1 = Number(document.getElementById("valor1").value);
+    const v2 = Number(document.getElementById("valor2").value);
+    const resultado = v1 / v2;
+
+    document.getElementById("resultado").innerText = `Resultado: ${resultado}`;
+}
+
+function restar(){
+    const v1 = Number(document.getElementById("valor1").value);
+    const v2 = Number(document.getElementById("valor2").value);
+    const resultado = v1 % v2;
+
+    document.getElementById("resultado").innerText = `Resultado: ${resultado}`;
+}
+
+function potenciar(){
+    const v1 = Number(document.getElementById("valor1").value);
+    const v2 = Number(document.getElementById("valor2").value);
+    const resultado = v1 ** v2;
+
+    document.getElementById("resultado").innerText = `Resultado: ${resultado}`;
+}
+
